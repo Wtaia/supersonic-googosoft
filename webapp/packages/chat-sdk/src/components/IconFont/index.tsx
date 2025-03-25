1ff992966111d4831b06503dcdfdd6d0fdde6095
门徒 <634041350@qq.com>
2025-03-25 16:06:16 +0800

    feat:cdn图标资源改为本地

--- webapp/packages/chat-sdk/src/components/IconFont/index.tsx
+++ webapp/packages/chat-sdk/src/components/IconFont/index.tsx
@@ -1,7 +1,7 @@
 import { createFromIconfontCN } from '@ant-design/icons';
 
 const IconFont = createFromIconfontCN({
-  scriptUrl: '//at.alicdn.com/t/c/font_4120566_7rwv3aw6wep.js',
+  scriptUrl: '/src/assets/icon/font_4120566_7rwv3aw6wep.js',
 });
 
 export default IconFont;
