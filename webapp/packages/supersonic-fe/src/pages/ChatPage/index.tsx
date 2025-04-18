384daeca2e8482c8335524315d351504d681219d
TAIA <taiacloud@qq.com>
2025-04-18 14:20:50 +0800

    fix:publicKey

--- webapp/packages/supersonic-fe/src/pages/ChatPage/index.tsx
+++ webapp/packages/supersonic-fe/src/pages/ChatPage/index.tsx
@@ -12,7 +12,7 @@ const ChatPage = () => {
   const [token, setToken] = useState(getToken() || '');
 
   useEffect(() => {
-    const publicKey = 'eyJhbGciOiJIUzUxMiJ9.eyJ0b2tlbl91c2VyX2VtYWlsIjoiYWRtaW5AeHguY29tIiwidG9rZW5fdXNlcl9pZCI6MSwidG9rZW5fdXNlcl9kaXNwbGF5X25hbWUiOiJhZG1pbiIsInRva2VuX2NyZWF0ZV90aW1lIjoxNzQzMDY5NTkxMzc0LCJ0b2tlbl9pc19hZG1pbiI6MSwidG9rZW5fdXNlcl9uYW1lIjoiYWRtaW4iLCJ0b2tlbl91c2VyX3Bhc3N3b3JkIjoiYzNWd1pYSnpiMjVwWTBCaWFXTnZiZGt0SkpZV3c2QTNyRW1CVVB6Ym4vNkROZVluRCt5M21Bd0RLRU1TM0tWVCIsInN1YiI6ImFkbWluIiwiZXhwIjoxNzcyMzIzNDA5fQ.z_521IB_bA2z7xTSp215i2x9T_K2KkkqSC1hWRxl6aGUrlRqhKVvRGzw3hyeQ6Ajd61M2hqX79_JIWt_zHaCdA';
+    const publicKey = 'eyJhbGciOiJIUzUxMiJ9.eyJ0b2tlbl91c2VyX2VtYWlsIjoiYWRtaW5AeHguY29tIiwidG9rZW5fdXNlcl9pZCI6MSwidG9rZW5fdXNlcl9kaXNwbGF5X25hbWUiOiJhZG1pbiIsInRva2VuX2NyZWF0ZV90aW1lIjoxNzQ0OTU0NTYxMDQxLCJ0b2tlbl9pc19hZG1pbiI6MSwidG9rZW5fdXNlcl9uYW1lIjoiYWRtaW4iLCJ0b2tlbl91c2VyX3Bhc3N3b3JkIjoiYzNWd1pYSnpiMjVwWTBCaWFXTnZiZGt0SkpZV3c2QTNyRW1CVVB6Ym4vNkROZVluRCt5M21Bd0RLRU1TM0tWVCIsInN1YiI6ImFkbWluIiwiZXhwIjoxODQ2MDIyMzk5fQ.nz_PRQScrEdctGU0uu0_s72umns4JLuZ5AdU-baflbUEsuzOi0p1kPStOw6bfRkMC84Li_ljIbNfPgE93hJyYQ';
     const query = queryString.parse(location.search) || {};
     const { agentId } = query;
 
