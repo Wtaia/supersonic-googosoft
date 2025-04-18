1b41639c90d82b2c89025ec0e7394f13076da5e8
TAIA <taiacloud@qq.com>
2025-04-18 17:41:59 +0800

    fix:sqlparse适配达梦

--- common/src/main/java/com/tencent/supersonic/common/calcite/SqlDialectFactory.java
+++ common/src/main/java/com/tencent/supersonic/common/calcite/SqlDialectFactory.java
@@ -15,7 +15,7 @@ public class SqlDialectFactory {
     public static final Context DEFAULT_CONTEXT =
             SqlDialect.EMPTY_CONTEXT.withDatabaseProduct(DatabaseProduct.BIG_QUERY)
                     .withLiteralQuoteString("'").withLiteralEscapedQuoteString("''")
-//                    .withIdentifierQuoteString("`")
+                    .withIdentifierQuoteString("\"")
                     .withUnquotedCasing(Casing.UNCHANGED)
                     .withQuotedCasing(Casing.UNCHANGED).withCaseSensitive(false);
     public static final Context POSTGRESQL_CONTEXT = SqlDialect.EMPTY_CONTEXT
